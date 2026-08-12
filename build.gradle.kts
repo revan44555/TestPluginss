007d8f61f9fcdc6393f948cc01443e750f8e45a1
Rəvan🇦🇿 <nazimqedirov300@gmail.com>
2026-08-12 20:04:12 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,11 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:81b1d424d2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
@@ -27,9 +25,17 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(
+    configuration: CloudstreamExtension.() -> Unit
+) = extensions
+    .getByName<CloudstreamExtension>("cloudstream")
+    .configuration()
 
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(
+    configuration: BaseExtension.() -> Unit
+) = extensions
+    .getByName<BaseExtension>("android")
+    .configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
@@ -37,8 +43,10 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "user/repo"
+        )
     }
 
     android {
@@ -57,7 +65,8 @@ subprojects {
 
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8) // Required
+                jvmTarget.set(JvmTarget.JVM_1_8)
+
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
@@ -71,18 +80,32 @@ subprojects {
         val cloudstream by configurations
         val implementation by configurations
 
-        // Stubs for all cloudstream classes
+        // CloudStream
         cloudstream("com.lagradost:cloudstream3:pre-release")
 
-        // These dependencies can include any of those which are added by the app,
-        // but you don't need to include any of them if you don't need them.
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
-        implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
-        implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
-        implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
-        // IMPORTANT: Do not bump Jackson above 2.13.1, as newer versions will
-        // break compatibility on older Android devices.
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // JSON Parser
+        // Kotlin
+        implementation(kotlin("stdlib"))
+
+        // HTTP
+        implementation(
+            "com.github.Blatzar:NiceHttp:0.4.11"
+        )
+
+        // HTML Parser
+        implementation(
+            "org.jsoup:jsoup:1.18.3"
+        )
+
+        // JSpecify nullability annotations
+        implementation(
+            "org.jspecify:jspecify:1.0.0"
+        )
+
+        // JSON Parser
+        // Do not bump Jackson above 2.13.1
+        implementation(
+            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
+        )
     }
 }
 
