18b7314bb34121ac025bc91b4f593fe78266d389
Rəvan🇦🇿 <nazimqedirov300@gmail.com>
2026-08-15 10:28:26 +0400

    Add files via upload

---
+++ NinjagoIzlesene/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 3
+
+cloudstream {
+    description = "Ninjago İzlesene — Ninjago bölümlerini izleme eklentisi"
+    authors = listOf("revan44555")
+    status = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=ninjagoizlesene.com.tr&sz=%size%"
+    language = "tr"
+}
